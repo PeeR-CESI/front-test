f7343edb8e8ef729e1bf4209e3b0e793b2860e5e
Antoine Spiers <antoine.spiers@gmail.com>
2024-04-09 10:05:34 +0200

    fix: prevent CI break for unused parameters

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
 import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router';
-import InscriptionPage from '../views/RegistrationView.vue'; // Assurez-vous d'importer correctement votre composant d'inscription
+import InscriptionPage from '../views/RegistrationView.vue';
 import LoginPage from '../views/LoginView.vue';
-import HomeView from "../views/HomeView.vue"; // Importez également le composant de connexion si ce n'est déjà fait
+import HomeView from "../views/HomeView.vue"; 
+import CreateServiceView from '../views/CreateServiceView.vue';
 
 const routes: Array<RouteRecordRaw> = [
     {
@@ -21,7 +22,20 @@ const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'Home',
-        component: HomeView, // Assurez-vous que HomeView est importé correctement
+        component: HomeView,
+        beforeEnter: (to, from, next) => {
+            // Vérifie si l'utilisateur a un token d'accès
+            if (localStorage.getItem('access_token')) {
+                next();
+            } else {
+                next({name: 'Login'}); // Redirige vers la page de connexion si aucun token n'est trouvé
+            }
+        },
+    },
+    {
+        path: '/service-creation',
+        name: 'Création d\'un service',
+        component: CreateServiceView,
         beforeEnter: (to, from, next) => {
             // Vérifiez si l'utilisateur a un token d'accès
             if (localStorage.getItem('access_token')) {
